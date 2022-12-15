74bb86457793e3f360c0a5568e393f64fdbf06f0
pradeep kumar biswal <99521716+pradeep-23ku@users.noreply.github.com>
2022-12-15 15:09:07 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+ function f1() {
+
+    let e= email.getText();
+    let p=password.getText();
+
+     if(email=="pintu" && password== "1234")
+
+     confirm("login sucessfully ");
+     else
+     alert("email & password doesn't match");
+
+ }
